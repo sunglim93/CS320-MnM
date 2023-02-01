8b2dadb32d4f412f5c06474092f9a62300fc195b
sunglim93 <122946316+sunglim93@users.noreply.github.com>
2023-01-31 20:35:11 -0800

    Revert "Add files via upload"
    This reverts commit 47c6014ec2575857e6760d9d3b9239fb04c20674.

--- MnM.py
+++ MnM.py
@@ -1,7 +1,6 @@
 import pygame
 import os
 import time
-import random
 pygame.init()
 
 WIDTH, HEIGHT = 900, 500
@@ -11,6 +10,7 @@ pygame.display.set_caption("Metal and Magic")
 FPS = 60
 
 clock = pygame.time.Clock()
+
 FONT = pygame.font.Font(os.path.join('Assets', 'alagard.ttf'), 52)
 QTE = pygame.USEREVENT + 1
 
@@ -27,62 +27,31 @@ def drawLetters(letterList):
 	x = 100
 	y = 100
 	for i in letterList:
-		WIN.blit(i, (x, y))
+		letter = i
+		WIN.blit(letter, (x, y))
 		x += 100
+	pygame.display.update()
 
-def drawCursor(curr):
-	x = 100
-	y = 50
-	WIN.blit((FONT.render('V', True, 'red')), (x+(100 * curr),y))
+def handleQTE(): 
+#To do:
+	#timer visualization
+	#randomized button combos
+	drawBlankWindow((255,255,255))  
+	letterList = []
+	text = "ZXC"
+	renderLetters(text, letterList)
+	drawLetters(letterList)
 
-def handleQTE():
-	choices = ['Z', 'X', 'C']
-	directions = ["UP", "DOWN", "LEFT", "RIGHT"]
-	text = [] 
-	combo = []
-	for i in range(5): #randomize a list of 5 letters
-		if (i % 2) != 0: #Z, X, C alternate with directional keys
-			letter = random.choice(choices)
-			text.append(letter)
-			if letter == 'Z': #generate combo list to check with
-				combo.append(pygame.K_z)
-			if letter == 'X':
-				combo.append(pygame.K_x)
-			if letter == 'C':
-				combo.append(pygame.K_c)
-		else:
-			direction = random.choice(directions)
-			if direction == "UP":
-				text.append('^')
-				combo.append(pygame.K_UP)
-			if direction == "DOWN":
-				text.append('V')
-				combo.append(pygame.K_DOWN)
-			if direction == "LEFT":
-				text.append('<')
-				combo.append(pygame.K_LEFT)
-			if direction == "RIGHT":
-				text.append('>')
-				combo.append(pygame.K_RIGHT)
-	letterList = [] #used to draw letters onto the screen
-	renderLetters(text, letterList) #generate letters to be drawn
 	curr = 0
+	combo = [pygame.K_z, pygame.K_x, pygame.K_c]
 	quick = True
-	timeDuration = 3000 #3 seconds to finish combo, will change with difficulty
-	timer = timeDuration
+	print("Starting QTE!")
+	time = 3000
 	while quick:
-		#draw screen, timer, and letters here
-		WIN.fill((255,255,255))
-		drawLetters(letterList)
-		drawCursor(curr)
-		timer -= clock.tick(FPS)
-		barWidth = (timer / timeDuration) * 400
-		pygame.draw.rect(WIN, 'red', (0,0,barWidth,50))
-		pygame.display.update()
-		pygame.time.delay(FPS)
-		if timer <= 0:
+		time -= clock.tick(FPS)
+		if time <= 0:
 			print("Failed! Ran out of time!")
-			drawBlankWindow((255,0,0)) #failure message
+			drawBlankWindow((255,0,0))
 			WIN.blit(FONT.render("Failure...", True, 'white'), (100,100))
 			pygame.display.update()
 			quick = False
@@ -92,14 +61,14 @@ def handleQTE():
 				if event.key == combo[curr]:
 					curr += 1
 					if curr == len(combo):
-						print("Success!") #success message
+						print("Success!")
 						drawBlankWindow((0,255,0))
 						WIN.blit(FONT.render("SUCCESS!", True, 'black'), (100,100))
 						pygame.display.update()
 
 						quick = False
 				else:
-					print("Failed! Wrong Combo!") #failure message
+					print("Failed! Wrong Combo!")
 					drawBlankWindow((255,0,0))
 					WIN.blit(FONT.render("Failure...", True, 'white'), (100,100))
 					pygame.display.update()
